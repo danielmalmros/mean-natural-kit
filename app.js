189cfd83592afb802c45102263ec89cf902c8880
Daniel Malmros <daniel@bymalmros.dk>
2018-02-04 14:57:09 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect to database
+mongoose.connect(config.database);
+
+// On connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database ' + config.database);
+})
+
+// On error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error ' + err);
+})
+
+const app = express();
+
+const users = require('./routes/users');
+
+// Port number
+const port = 3000;
+
+// CORS Middelware
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middelware
+app.use(bodyParser.json());
+
+app.use('/users', users)
+
+// Index Route
+app.get('/', (req, res) => {
+    res.send('Invalid endport');
+});
+
+// Start router
+app.listen(port, () => {
+    console.log('Server startet on port: ' + port);
+});
